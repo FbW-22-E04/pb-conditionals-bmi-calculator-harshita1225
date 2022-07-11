86e737f6b3aa4869e26ff39808114af20961883d
harshita <harshita1225@gmail.com>
2022-07-11 13:32:30 +0200

    task completed

--- solution.js
+++ solution.js
@@ -1,12 +1,22 @@
 //Q1 Store Mark's and John's mass and height in variables.
 
+let massMark = 95;  //weight is in kgs
+let heightMark = 1.82 //height in meters
 
+let massJohn = 88;
+let heightJohn = 1.68;
 
 //Q2 Calculate both their BMIs and store their BMIs in variables. 
 
 
 
+const bmiMark = massMark / (heightMark * heightMark) ;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+
 //Q3 Create a boolean variable containing information about whether Mark has a higher BMI than John.
+
+const trueValue = "true";
+const falseValue = "false";
 
 
 
@@ -14,5 +24,15 @@
 
 
 
+console.log("Is Mark's BMI higher than John's? " ,bmiMark > bmiJohn ?   `yes, it's ${trueValue}, value is ${bmiMark}` :`no it is ${falseValue}`);
+
 //Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
+
+if(bmiMark > bmiJohn ){
+
+    console.log(`BMI of mark ${bmiMark} is greater than BMI of John ${bmiJohn}`);
+}
+else{
+    console.log(`BMI of John ${bmiJohn} is greater than BMI of Mark ${bmiMark}`);
+}
 
